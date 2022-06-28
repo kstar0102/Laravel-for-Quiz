2f18708ba34031eadd0faacb8289b5469ef6eea4
valdim.expert@mail.ru <valdim.expert@mail.ru>
2022-06-29 03:50:58 +0900

    create question model and data tabel

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions(){
+        return $this-hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->arttributes['slug'] = Str::slug($value);
+    }
 }
